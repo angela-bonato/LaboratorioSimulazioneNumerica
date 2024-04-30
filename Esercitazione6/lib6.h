a45618c57143785c8fa2bf68f491d805180c128b
Angela Bonato <129956310+angela-bonato@users.noreply.github.com>
2024-04-30 18:00:56 +0200

    Delete directory with wrong name

--- Esercitazione6/lib6.h
+++
@@ -1,8 +0,0 @@
-#include "SOURCE/system.h"
-
-using namespace std;
-
-// Funzione che esegue il ciclo di equilibrazione del sistema misurando l'energia totale del sistema, flag p serve a capire se registrare o no i dati di equilibrazione per il relativo plot
-void Equilibration(System& syst, bool p);
-// Funzione che esegue il ciclo di analisi del sistema misurando le proprietà richieste nel file properties.dat
-void Analysis(System& syst); 
